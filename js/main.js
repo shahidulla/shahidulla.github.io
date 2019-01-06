e736b1448414d24b76cd1797ddfa0097e59437ad
Md Shahidulla <shahid.ruetete@gmail.com>
2019-01-05 20:54:19 -0600

    initial check in

---
+++ js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  $("#para").on('click', function(){
+      $(this).css("color", "black");
+  });
+  $("body").on('click', function(){
+    $(this).css("background-color", "white");
+  });
+})
